85003dd131f3817e6b1d74a9b875144d1310f171
Kyle Tulod <tulod.kyle.m@gmail.com>
2016-12-13 16:34:54 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,19 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    # songs.first.genre #<-----WORKS ALSO!!!
+      Song.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    # songs.count #<-----WORKS ALSO!!!
+    Song.select(:artist).size
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    # songs.collect {|song| song.genre}.uniq.size #<-----WORKS ALSO!!!
+    Genre.joins(:songs, :artists).size
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,19 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    # songs.length #<-----WORKS ALSO!!!
+    Song.select(:genre).size
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    # self.all.collect {|genre| genre.artists}.flatten.size #<-----WORKS ALSO!!!
+    Artist.select(:genre).size
+
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    Artist.all.collect {|artist| artist.name}
   end
 end
